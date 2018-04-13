9dd199d07c81c6789b874e4f77632f74f207b844
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-04-13 16:38:43 -0400

    Disable firebase until later; add basic moment.js from lecture examples

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,23 +1,35 @@
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyA4BxBf9kG3jICJYjZbwPkuqpnjBqOkhjQ",
-    authDomain: "csl-hw7.firebaseapp.com",
-    databaseURL: "https://csl-hw7.firebaseio.com",
-    projectId: "csl-hw7",
-    storageBucket: "csl-hw7.appspot.com",
-    messagingSenderId: "670394771122"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+// var config = {
+//     apiKey: "AIzaSyA4BxBf9kG3jICJYjZbwPkuqpnjBqOkhjQ",
+//     authDomain: "csl-hw7.firebaseapp.com",
+//     databaseURL: "https://csl-hw7.firebaseio.com",
+//     projectId: "csl-hw7",
+//     storageBucket: "csl-hw7.appspot.com",
+//     messagingSenderId: "670394771122"
+// };
+// firebase.initializeApp(config);
+// var database = firebase.database();
 
 // other global variables
-var dbName = "trains";
+// var dbName = "trains";
 
 
 
 // jQuery wrapper
 $(document).ready(function () {
     console.log("ready!");
+
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
+
+    // *** start from lecture
+    // Time is 3:30 AM
+    var firstTime = "03:30";
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "day");
+    console.log(firstTimeConverted);
+    // *** end from lecture
 
     // grab DOM elements
     var trainsOutput = $('#trains-output');
@@ -31,6 +43,10 @@ $(document).ready(function () {
         var inputFirstTime = $('#input-first-time').val().trim();
         var inputFrequency = $('#input-frequency').val().trim();
         console.log(inputTrainName, inputDestination, inputFirstTime, inputFrequency);
+
+        console.log( moment(inputFirstTime) );
+
+   
 
         var newTR = $('<tr>');
         [inputTrainName, inputDestination, inputFrequency].forEach(element => {
