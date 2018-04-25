4ff3be89e797a33082942dc7e3b734c79402c1a2
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-04-25 16:50:42 -0400

    Update momentjs and firebase script versions

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,17 +1,17 @@
 // Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyA4BxBf9kG3jICJYjZbwPkuqpnjBqOkhjQ",
-//     authDomain: "csl-hw7.firebaseapp.com",
-//     databaseURL: "https://csl-hw7.firebaseio.com",
-//     projectId: "csl-hw7",
-//     storageBucket: "csl-hw7.appspot.com",
-//     messagingSenderId: "670394771122"
-// };
-// firebase.initializeApp(config);
-// var database = firebase.database();
+var config = {
+    apiKey: "AIzaSyA4BxBf9kG3jICJYjZbwPkuqpnjBqOkhjQ",
+    authDomain: "csl-hw7.firebaseapp.com",
+    databaseURL: "https://csl-hw7.firebaseio.com",
+    projectId: "csl-hw7",
+    storageBucket: "csl-hw7.appspot.com",
+    messagingSenderId: "670394771122"
+  };
+  firebase.initializeApp(config);
+var database = firebase.database();
 
 // other global variables
-// var dbName = "trains";
+var dbName = "trains";
 
 
 
